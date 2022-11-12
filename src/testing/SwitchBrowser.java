cdd7a0ad13472b905c01bb43e344cc9a4ddb837c
Lourdes Dinesh <lourdes.dinesh@eka.care>
2022-11-12 21:16:01 +0530

    Added SwitchBrowser support for Sarari

--- src/testing/SwitchBrowser.java
+++ src/testing/SwitchBrowser.java
@@ -1,10 +1,14 @@
 package testing;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 public class SwitchBrowser {
 	public static void toChromeDriver() {
 		MainClass.driver = new ChromeDriver();
 	}
 
+	public static void toSafari() {
+        MainClass.driver = new SafariDriver();
+    }
 }
